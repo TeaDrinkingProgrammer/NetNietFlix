b45929eea673d38abef78516d0d30f14f3feb574
Stijn van Houwelingen <stijn@van-houwelingen.net>
2021-04-02 14:20:02 +0200

    Abstracted the API via DataController and DataManager to enable easier implementation of the database later on.

--- NetNietFlix+/app/src/main/java/nl/avans/netnietflix/applogic/DataManager.java
+++ NetNietFlix+/app/src/main/java/nl/avans/netnietflix/applogic/DataManager.java
@@ -1,4 +1,24 @@
 package nl.avans.netnietflix.applogic;
 
+import nl.avans.netnietflix.repository.API.DetailMediaItemController;
+import nl.avans.netnietflix.repository.API.MediaItemControllerListener;
+import nl.avans.netnietflix.repository.DataController;
+
 public class DataManager {
+    private static final String TAG = DataManager.class.getSimpleName();
+    private DataController dataController;
+
+    public DataManager(){
+        dataController = new DataController();
+    }
+
+    public void loadTrendingMediaItems(MediaItemControllerListener listener){
+        dataController.loadTrendingMediaItems(listener);
+    }
+    public void loadTopRatedMediaItems(MediaItemControllerListener listener){
+        dataController.loadTopRatedMediaItems(listener);
+    }
+    public void getMediaItemDetails(DetailMediaItemController.DetailedMediaItemListener listener, int mediaItemid){
+        dataController.getMediaItemDetails(listener, mediaItemid);
+    }
 }
--- NetNietFlix+/app/src/main/java/nl/avans/netnietflix/repository/DataController.java
+++ NetNietFlix+/app/src/main/java/nl/avans/netnietflix/repository/DataController.java
@@ -1,4 +1,47 @@
 package nl.avans.netnietflix.repository;
 
-public class DataController {
-}
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import java.io.IOException;
+
+import nl.avans.netnietflix.repository.API.DetailMediaItemController;
+import nl.avans.netnietflix.repository.API.MediaItemControllerListener;
+import nl.avans.netnietflix.repository.API.TopRatedMediaItemController;
+import nl.avans.netnietflix.repository.API.TrendingMediaItemController;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class DataController{
+    private static final String TAG = DataController.class.getSimpleName();
+    public void loadTrendingMediaItems(MediaItemControllerListener listener){
+        // Do an asynchronous operation to fetch movies
+        Log.d(TAG, "loadMediaItems from API");
+        TrendingMediaItemController apiController = new TrendingMediaItemController(listener);
+        apiController.loadTrendingMoviesPerWeek();
+    }
+    public void loadTopRatedMediaItems(MediaItemControllerListener listener){
+        // Do an asynchronous operation to fetch movies
+        Log.d(TAG, "loadMediaItems from API");
+        TopRatedMediaItemController apiController = new TopRatedMediaItemController(listener);
+        apiController.loadTopRatedMoviesPerWeek();
+    }
+    public void getMediaItemDetails(DetailMediaItemController.DetailedMediaItemListener listener, int mediaItemid){
+        DetailMediaItemController apiController = new DetailMediaItemController(listener);
+        apiController.getMediaItemDetails(mediaItemid);
+    }
+    public boolean isConnected() {
+        try {
+            String command = "ping -c 1 api.themoviedb.org";
+            return Runtime.getRuntime().exec(command).waitFor() == 0;
+        } catch (InterruptedException ie) {
+            Log.d(TAG, "Error while checking connection: IO Exception:" + ie.getStackTrace());
+        } catch (IOException io) {
+            Log.d(TAG, "Error while checking connection: IO Exception:" + io.getStackTrace());
+        }
+        return false;
+    }
+
+    }
